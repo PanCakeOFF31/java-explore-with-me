cfb56132db33caa2a668728c9f6aa0af9378460b
Maxim <mak.blinov.original@yandex.ru>
2024-06-19 13:55:47 +0300

    commit

--- stats-service/http-service/src/test/java/ru/practicum/StatsServerTest.java
+++ stats-service/http-service/src/test/java/ru/practicum/StatsServerTest.java
@@ -1,7 +1,6 @@
 package ru.practicum;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
